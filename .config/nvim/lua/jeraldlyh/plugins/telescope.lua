fe0a6335c1df62e316a58dae3218af48d54fca3f
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2024-08-27 11:45:30 +0800

    perf(nvim): implement selection of directory for grep

--- .config/nvim/lua/jeraldlyh/plugins/telescope.lua
+++ .config/nvim/lua/jeraldlyh/plugins/telescope.lua
@@ -6,10 +6,39 @@ return {
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     "nvim-tree/nvim-web-devicons",
     "folke/todo-comments.nvim",
+    "nvim-telescope/telescope-file-browser.nvim",
   },
   config = function()
     local telescope = require("telescope")
     local actions = require("telescope.actions")
+
+    local ts_select_dir_for_grep = function(prompt_bufnr)
+      local action_state = require("telescope.actions.state")
+      local fb = require("telescope").extensions.file_browser
+      local live_grep = require("telescope.builtin").live_grep
+      local current_line = action_state.get_current_line()
+
+      fb.file_browser({
+        files = false,
+        depth = false,
+        attach_mappings = function(prompt_bufnr)
+          require("telescope.actions").select_default:replace(function()
+            local entry_path = action_state.get_selected_entry().Path
+            local dir = entry_path:is_dir() and entry_path or entry_path:parent()
+            local relative = dir:make_relative(vim.fn.getcwd())
+            local absolute = dir:absolute()
+
+            live_grep({
+              results_title = relative .. "/",
+              cwd = absolute,
+              default_text = current_line,
+            })
+          end)
+
+          return true
+        end,
+      })
+    end
 
     telescope.setup({
       defaults = {
@@ -19,6 +48,10 @@ return {
             ["<C-k>"] = actions.move_selection_previous,
             ["<C-j>"] = actions.move_selection_next,
             ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+            ["<C-f>"] = ts_select_dir_for_grep,
+          },
+          n = {
+            ["<C-f>"] = ts_select_dir_for_grep,
           },
         },
       },
