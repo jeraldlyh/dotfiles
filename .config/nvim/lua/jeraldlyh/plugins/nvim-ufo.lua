c0178cb736f62c2af1376d40a1a9010eb60aef82
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2024-10-11 16:19:24 +0800

    perf(nvim): disable fold column

--- .config/nvim/lua/jeraldlyh/plugins/nvim-ufo.lua
+++ .config/nvim/lua/jeraldlyh/plugins/nvim-ufo.lua
@@ -5,7 +5,7 @@ return {
     event = "BufReadPost",
     init = function()
       vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
-      vim.o.foldcolumn = "1"
+      -- vim.o.foldcolumn = "1"
       vim.o.foldlevel = 99
       vim.o.foldenable = true
       vim.o.foldlevelstart = 99
