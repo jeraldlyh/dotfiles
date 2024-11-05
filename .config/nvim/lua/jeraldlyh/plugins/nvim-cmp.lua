5f2797b72b8e45b25cb9376effb1604249dfa76c
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2024-11-05 17:24:10 +0800

    perf(nvim): remove all snippet code suggestion

--- .config/nvim/lua/jeraldlyh/plugins/nvim-cmp.lua
+++ .config/nvim/lua/jeraldlyh/plugins/nvim-cmp.lua
@@ -4,31 +4,15 @@ return {
   dependencies = {
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-path",
-    {
-      "L3MON4D3/LuaSnip",
-      version = "v2.*",
-      build = "make install_jsregexp",
-    },
-    "saadparwaiz1/cmp_luasnip",
     "onsails/lspkind.nvim",
   },
   config = function()
     local cmp = require("cmp")
-
-    local luasnip = require("luasnip")
-
     local lspkind = require("lspkind")
-
-    require("luasnip.loaders.from_vscode").lazy_load()
 
     cmp.setup({
       completion = {
         completeopt = "menu,menuone,preview,noselect",
-      },
-      snippet = {
-        expand = function(args)
-          luasnip.lsp_expand(args.body)
-        end,
       },
       mapping = cmp.mapping.preset.insert({
         ["<C-k>"] = cmp.mapping.select_prev_item(),
@@ -40,8 +24,12 @@ return {
         ["<CR>"] = cmp.mapping.confirm({ select = false }),
       }),
       sources = cmp.config.sources({
-        { name = "nvim_lsp" },
-        { name = "luasnip" },
+        {
+          name = "nvim_lsp",
+          entry_filter = function(entry, ctx)
+            return require("cmp").lsp.CompletionItemKind.Snippet ~= entry:get_kind()
+          end,
+        },
         { name = "buffer" },
         { name = "path" },
       }),
