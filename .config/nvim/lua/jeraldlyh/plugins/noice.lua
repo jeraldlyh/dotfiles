d31a1f6830e091444e9d9eb67e0006c92f45628d
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2024-12-09 23:37:12 +0800

    perf(nvim): suppress no information available message in noice

--- .config/nvim/lua/jeraldlyh/plugins/noice.lua
+++ .config/nvim/lua/jeraldlyh/plugins/noice.lua
@@ -8,23 +8,35 @@ return {
       opts = { top_down = false },
     },
   },
-  opts = function(_, opts)
-    opts.presets = {
-      command_palette = {
-        views = {
-          cmdline_popup = {
-            position = {
-              row = "50%",
-              col = "50%",
-            },
-            size = {
-              min_width = 60,
-              width = "auto",
-              height = "auto",
+  config = function()
+    require("noice").setup({
+      routes = {
+        {
+          filter = {
+            event = "notify",
+            find = "No information available",
+          },
+          opts = { skip = true },
+        },
+      },
+      presets = {
+        command_palette = {
+          views = {
+            cmdline_popup = {
+              position = {
+                row = "50%",
+                col = "50%",
+              },
+              size = {
+                min_width = 60,
+                width = "auto",
+                height = "auto",
+              },
             },
           },
         },
+        lsp_doc_border = true,
       },
-    }
+    })
   end,
 }
