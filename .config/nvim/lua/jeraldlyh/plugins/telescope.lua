0cbec7caa5d8dafdba6bc7f40fc9462113e7af37
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2025-03-10 17:10:56 +0800

    fix(nvim): use native fzf for telescope

--- .config/nvim/lua/jeraldlyh/plugins/telescope.lua
+++ .config/nvim/lua/jeraldlyh/plugins/telescope.lua
@@ -41,51 +41,27 @@ return {
     end
 
     telescope.setup({
+      pickers = {
+        live_grep = {
+          file_ignore_patterns = { "node_modules", ".git", ".venv" },
+          additional_args = function(_)
+            return { "--hidden" }
+          end,
+        },
+        find_files = {
+          hidden = true,
+          file_ignore_patterns = { "node_modules", ".git", ".venv" },
+        },
+      },
+      path_display = function(opts, path)
+        local tail = require("telescope.utils").path_tail(path)
+        path = string.format("%s (%s)", tail, path)
+
+        local highlights = { { { 0, #path }, "Comment" } }
+
+        return path, highlights
+      end,
       defaults = {
-        vimgrep_arguments = {
-          "rg",
-          "--follow", -- Follow symbolic links
-          "--hidden", -- Search for hidden files
-          "--no-heading", -- Don't group matches by each file
-          "--with-filename", -- Print the file path with the matched lines
-          "--line-number", -- Show line numbers
-          "--column", -- Show column numbers
-          "--smart-case", -- Smart case search
-
-          -- Exclude some patterns from search
-          "--glob=!**/.git/*",
-          "--glob=!**/.idea/*",
-          "--glob=!**/.vscode/*",
-          "--glob=!**/build/*",
-          "--glob=!**/dist/*",
-          "--glob=!**/yarn.lock",
-          "--glob=!**/package-lock.json",
-        },
-        pickers = {
-          find_files = {
-            hidden = true,
-            find_command = {
-              "rg",
-              "--files",
-              "--hidden",
-              "--glob=!**/.git/*",
-              "--glob=!**/.idea/*",
-              "--glob=!**/.vscode/*",
-              "--glob=!**/build/*",
-              "--glob=!**/dist/*",
-              "--glob=!**/yarn.lock",
-              "--glob=!**/package-lock.json",
-            },
-          },
-        },
-        path_display = function(opts, path)
-          local tail = require("telescope.utils").path_tail(path)
-          path = string.format("%s (%s)", tail, path)
-
-          local highlights = { { { 0, #path }, "Comment" } }
-
-          return path, highlights
-        end,
         mappings = {
           i = {
             ["<C-k>"] = actions.move_selection_previous,
@@ -98,9 +74,9 @@ return {
             ["<C-i>"] = actions.smart_send_to_qflist + actions.open_qflist,
           },
         },
-        file_ignore_patterns = {
-          "venv",
-        },
+      },
+      extensions = {
+        "fzf",
       },
     })
     telescope.load_extension("fzf")
