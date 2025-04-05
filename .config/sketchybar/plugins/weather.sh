b863b8cd2975cfb93d349b3678eb0b74733309ff
Jerald Lim <jeraldssw@gmail.com>
2025-04-05 16:27:26 +0800

    fix(sketchybar): malformed utf8 string

--- .config/sketchybar/plugins/weather.sh
+++ .config/sketchybar/plugins/weather.sh
@@ -4,20 +4,16 @@ sketchybar --set $NAME \
   label="Loading..." \
   icon.color=0xff5edaff
 
-LOCATION="Singapore"
-REGION=""
+WEATHER_DATA=$(curl -s "https://wttr.in/Singapore?format=%c+%f")
 
-# Replace spaces with +
-LOCATION_ESCAPED="${LOCATION// /+}+${REGION// /+}"
-WEATHER_DATA=$(curl -s "https://wttr.in/$LOCATION_ESCAPED?format=%c%f")
-
-# echo "https://wttr.in/$LOCATION_ESCAPED?0pq&format=j1&lang=en"
-if [[  -z "$WEATHER_DATA" || "$WEATHER_DATA" =~ "try" ]]; then
-  sketchybar --set $NAME label="$LOCATION" icon.y_offset=0
+if [[ -z "$WEATHER_DATA" || "$WEATHER_DATA" =~ "try" ]]; then
+  sketchybar --set $NAME label="N/A" icon.y_offset=0
   exit
 fi
 
-LABEL="${WEATHER_DATA:1:${#WEATHER_DATA}-2}"
-ICON="$(echo "${WEATHER_DATA:0:1}")"
+ICON=$(echo "$WEATHER_DATA" | cut -d' ' -f1)
+LABEL=$(echo "$WEATHER_DATA" | cut -d' ' -f2-)
+
+ICON=$(echo "$ICON" | sed 's/️//g')  # Removes invisible variation selector if present
 
 sketchybar --set $NAME label="$LABEL" icon="$ICON" icon.y_offset=2
