1f8d49f679dcd711c8242c043494b0f68a062a12
Jerald Lim <jeraldssw@gmail.com>
2025-08-24 15:58:17 +0800

    perf(nvim): disable name colors for nvim-colorizer

--- .config/nvim/lua/jeraldlyh/plugins/editor/nvim-colorizer.lua
+++ .config/nvim/lua/jeraldlyh/plugins/editor/nvim-colorizer.lua
@@ -1,6 +1,16 @@
 return {
   "norcalli/nvim-colorizer.lua",
   config = function()
-    require("colorizer").setup()
+    require("colorizer").setup({ "*" }, {
+      RGB = true,
+      RRGGBB = true,
+      names = false,
+      RRGGBBAA = true,
+      rgb_fn = true,
+      hsl_fn = true,
+      css = false,
+      css_fn = true,
+      mode = "background",
+    })
   end,
 }
