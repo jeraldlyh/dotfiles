88d56eaa87b113e21d391be3f8e97810e701543e
Jerald Lim <jeraldssw@gmail.com>
2025-11-01 16:14:11 +0800

    perf(nvim): create keybind for react template

--- .config/nvim/after/lsp/vtsls.lua
+++ .config/nvim/after/lsp/vtsls.lua
@@ -1,3 +1,48 @@
+local function create_react_file()
+  vim.ui.input({ prompt = "Enter component name: " }, function(name)
+    if not name or name == "" then
+      return
+    end
+    local filename = string.lower(name) .. ".tsx"
+    local template = string.format(
+      [[
+import React from "react";
+
+export const %s = () => {
+  /* ---------------------------------------------------------- */
+  /*                      CONST STATE REF                       */
+  /* ---------------------------------------------------------- */
+
+  /* ---------------------------------------------------------- */
+  /*                          EFFECTS                           */
+  /* ---------------------------------------------------------- */
+
+  /* ---------------------------------------------------------- */
+  /*                      HELPER FUNCTIONS                      */
+  /* ---------------------------------------------------------- */
+
+  /* ---------------------------------------------------------- */
+  /*                           RENDER                           */
+  /* ---------------------------------------------------------- */
+
+  return (<div></div>)
+};
+]],
+      name
+    )
+    local cwd = vim.fn.expand("%:p:h")
+    local path = cwd .. "/" .. filename
+    local file = io.open(path, "w")
+    if file then
+      file:write(template)
+      file:close()
+      vim.cmd("edit " .. path)
+    else
+      vim.notify("Could not create file: " .. path, vim.log.levels.ERROR)
+    end
+  end)
+end
+
 return {
   on_attach = function(_, bufnr)
     local function organize_imports()
@@ -9,6 +54,7 @@ return {
     end
 
     vim.keymap.set("n", "<leader>oi", organize_imports, { desc = "Organize imports", buffer = bufnr })
+    vim.keymap.set("n", "<leader>rc", create_react_file, { desc = "Create React file", buffer = bufnr })
   end,
   settings = {
     typescript = {
