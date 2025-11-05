dc91895d83932461e59deb604a54321cf12f444d
Jerald Lim <jeraldssw@gmail.com>
2025-11-05 11:38:25 +0800

    perf(nvim): add schema store for yamlls

--- .config/nvim/after/lsp/yamlls.lua
+++ .config/nvim/after/lsp/yamlls.lua
@@ -3,6 +3,15 @@ return {
     yaml = {
       customTags = {
         "!reference sequence",
+        "!reference mapping",
+        "!reference scalar",
+      },
+      schemaStore = {
+        url = "https://www.schemastore.org/api/json/catalog.json",
+        enable = true,
+      },
+      schemas = {
+        ["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = ".gitlab-ci.{yml,yaml}",
       },
     },
   },
