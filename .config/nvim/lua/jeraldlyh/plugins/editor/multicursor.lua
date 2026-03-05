265a2a3840f3a19bcc688614279f0c9f4c94a28f
Jerald Lim <jeraldssw@gmail.com>
2026-03-05 18:18:28 +0800

    perf(nvim): replace vim-visual-multi with multicursor

---
+++ .config/nvim/lua/jeraldlyh/plugins/editor/multicursor.lua
@@ -0,0 +1,62 @@
+return {
+  "jake-stewart/multicursor.nvim",
+  branch = "1.0",
+  config = function()
+    local multicursor = require("multicursor-nvim")
+    local keymap = vim.keymap
+
+    multicursor.setup()
+
+    keymap.set({ "n", "x" }, "<up>", function()
+      multicursor.lineAddCursor(-1)
+    end)
+    keymap.set({ "n", "x" }, "<down>", function()
+      multicursor.lineAddCursor(1)
+    end)
+    keymap.set({ "n", "x" }, "<leader><up>", function()
+      multicursor.lineSkipCursor(-1)
+    end)
+    keymap.set({ "n", "x" }, "<leader><down>", function()
+      multicursor.lineSkipCursor(1)
+    end)
+
+    keymap.set({ "n", "x" }, "<leader>n", function()
+      multicursor.matchAddCursor(1)
+    end, { desc = "Add next cursor by matching word/selection" })
+    keymap.set({ "n", "x" }, "<leader>s", function()
+      multicursor.matchSkipCursor(1)
+    end, { desc = "Skip next cursor by matching word/selection" })
+    keymap.set({ "n", "x" }, "<leader>N", function()
+      multicursor.matchAddCursor(-1)
+    end, { desc = "Add previous cursor by matching word/selection" })
+    keymap.set({ "n", "x" }, "<leader>S", function()
+      multicursor.matchSkipCursor(-1)
+    end, { desc = "Skip previous cursor by matching word/selection" })
+
+    keymap.set("n", "<c-leftmouse>", multicursor.handleMouse)
+    keymap.set("n", "<c-leftdrag>", multicursor.handleMouseDrag)
+    keymap.set("n", "<c-leftrelease>", multicursor.handleMouseRelease)
+
+    multicursor.addKeymapLayer(function(layerSet)
+      layerSet({ "n", "x" }, "<left>", multicursor.prevCursor, { desc = "Select previous cursor" })
+      layerSet({ "n", "x" }, "<right>", multicursor.nextCursor, { desc = "Select next cursor" })
+      layerSet({ "n", "x" }, "<leader>x", multicursor.deleteCursor, { desc = "Delete cursor" })
+      layerSet("n", "<esc>", function()
+        if not multicursor.cursorsEnabled() then
+          multicursor.enableCursors()
+        else
+          multicursor.clearCursors()
+        end
+      end, { desc = "Exit multi-cursor mode" })
+    end)
+
+    local hl = vim.api.nvim_set_hl
+    hl(0, "MultiCursorCursor", { reverse = true })
+    hl(0, "MultiCursorVisual", { link = "Visual" })
+    hl(0, "MultiCursorSign", { link = "SignColumn" })
+    hl(0, "MultiCursorMatchPreview", { link = "Search" })
+    hl(0, "MultiCursorDisabledCursor", { reverse = true })
+    hl(0, "MultiCursorDisabledVisual", { link = "Visual" })
+    hl(0, "MultiCursorDisabledSign", { link = "SignColumn" })
+  end,
+}
--- .config/nvim/lua/jeraldlyh/plugins/editor/vim-visual-multi.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "mg979/vim-visual-multi",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    vim.keymap.set("n", "<C-m>", "<Plug>(VM-Add-Cursor-Down)", { noremap = true, silent = true })
-  end,
-}
