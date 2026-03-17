9c2a174173d57ade07a9c1bffe895b0eb28240ef
Jerald Lim <jeraldssw@gmail.com>
2026-03-17 12:19:10 +0800

    fix(nvim): customise lua_ls for diagnostics

---
+++ .config/nvim/after/lsp/lua_ls.lua
@@ -0,0 +1,33 @@
+local root_markers_primary = {
+  ".emmyrc.json",
+  ".luarc.json",
+  ".luarc.jsonc",
+}
+local root_markers_secondary = {
+  ".luacheckrc",
+  ".stylua.toml",
+  "stylua.toml",
+  "selene.toml",
+  "selene.yml",
+}
+return {
+  cmd = { "lua-language-server" },
+  filetypes = { "lua" },
+  root_markers = vim.fn.has("nvim-0.11.3") == 1 and { root_markers_primary, root_markers_secondary, { ".git" } }
+    or vim.list_extend(vim.list_extend(root_markers_primary, root_markers_secondary), { ".git" }),
+
+  ---@type lspconfig.settings.lua_ls
+  settings = {
+    Lua = {
+      codeLens = { enable = true },
+      hint = { enable = true, semicolon = "Disable" },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {
+          "vim",
+          "require",
+        },
+      },
+    },
+  },
+}
