29eb44cfae4b88ef09f937e65e53c4a43c000047
Jerald Lim <jeraldssw@gmail.com>
2026-04-23 13:53:59 +0800

    perf(nvim): save chat upon starting new sessions for sidekick

--- .config/nvim/lua/jeraldlyh/plugins/ai/sidekick.lua
+++ .config/nvim/lua/jeraldlyh/plugins/ai/sidekick.lua
@@ -1,3 +1,41 @@
+local send_enter_key = function(delay)
+  local pane = vim.env.TMUX_PANE
+
+  if not pane or pane == "" then
+    vim.notify("TMUX_PANE not set", vim.log.levels.ERROR)
+    return
+  end
+
+  vim.defer_fn(function()
+    vim.fn.system({
+      "tmux",
+      "send-keys",
+      "-t",
+      vim.env.TMUX_PANE,
+      "Enter",
+    })
+  end, delay or 100)
+end
+
+local save_chat = function()
+  local save_dir = vim.fn.expand("$HOME/Documents/Personal/ai/sidekick-chat")
+  vim.fn.mkdir(save_dir, "p")
+
+  local filename = save_dir .. "/" .. os.date("%Y-%m-%d_%H-%M-%S") .. ".md"
+
+  require("sidekick.cli").send({
+    msg = "/share file " .. filename,
+  })
+
+  send_enter_key(100)
+end
+
+local start_new_chat = function()
+  require("sidekick.cli").send({ msg = "/new" })
+
+  send_enter_key(200)
+end
+
 return {
   "folke/sidekick.nvim",
   opts = {
@@ -23,11 +61,25 @@ return {
       desc = "Goto/apply next edit suggestion",
     },
     {
+      "<leader>cn",
+      function()
+        save_chat()
+        start_new_chat()
+      end,
+      mode = { "n", "x" },
+      desc = "Start new chat",
+    },
+    {
       "<leader>cc",
       function()
-        require("sidekick.cli").close()
+        save_chat()
+        start_new_chat()
+
+        vim.defer_fn(function()
+          require("sidekick.cli").close()
+        end, 6000)
       end,
-      desc = "Detach a CLI Session",
+      desc = "Close and save chat",
     },
     {
       "<leader>cl",
