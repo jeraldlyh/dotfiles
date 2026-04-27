1eeec22f6511c3237c3968f3dc2112e002ced6fa
Jerald Lim <jeraldssw@gmail.com>
2026-04-27 21:38:34 +0800

    perf(nvim): create floating terminal plugin

--- .config/nvim/lua/jeraldlyh/core/keymaps.lua
+++ .config/nvim/lua/jeraldlyh/core/keymaps.lua
@@ -24,8 +24,6 @@ keymap.set("n", "<leader>sl", "<cmd>vertical resize -5<cr>", { desc = "Decrease 
 keymap.set("n", "<leader>sh", "<cmd>vertical resize +5<cr>", { desc = "Increase width" })
 keymap.set("n", "<leader>sj", "<cmd>resize +5<cr>", { desc = "Increase height" })
 keymap.set("n", "<leader>sk", "<cmd>resize -5<cr>", { desc = "Decrease height" })
-keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down and center" })
-keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up and center" })
 keymap.set("n", "<C-e>", "3<C-e>", { desc = "Scroll down" })
 keymap.set("n", "<C-y>", "3<C-y>", { desc = "Scroll up" })
 
