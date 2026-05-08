6c9206f02c26b6e52436b23c540c868e1471b1f1
Jerald Lim <jeraldssw@gmail.com>
2026-05-08 22:37:18 +0800

    perf(nvim): enable auto-session

--- .config/nvim/lua/jeraldlyh/plugins/utils/auto-session.lua
+++ .config/nvim/lua/jeraldlyh/plugins/utils/auto-session.lua
@@ -1,12 +1,31 @@
 return {
   "rmagatti/auto-session",
-  enabled = false,
+  enabled = true,
   config = function()
     local auto_session = require("auto-session")
     local keymap = vim.keymap
 
+    local function restore_filetypes()
+      vim.schedule(function()
+        for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+          if vim.api.nvim_buf_is_loaded(buf) then
+            local buftype = vim.bo[buf].buftype
+            local name = vim.api.nvim_buf_get_name(buf)
+            local filetype = vim.bo[buf].filetype
+
+            if buftype == "" and name ~= "" and filetype == "" then
+              vim.api.nvim_buf_call(buf, function()
+                vim.cmd("silent! filetype detect")
+              end)
+            end
+          end
+        end
+      end)
+    end
+
     auto_session.setup({
       suppressed_dirs = { "~/" },
+      post_restore_cmds = { restore_filetypes },
     })
 
     keymap.set("n", "<leader>wr", "<cmd>SessionRestore<cr>", { desc = "Restore session" })
