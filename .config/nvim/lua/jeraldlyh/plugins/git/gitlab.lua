9303550d3578053808b4f3b047c716540da63c43
Jerald Lim <jeraldssw@gmail.com>
2026-08-12 14:14:58 +0800

    perf(nvim): install gitlab plugin

---
+++ .config/nvim/lua/jeraldlyh/plugins/git/gitlab.lua
@@ -0,0 +1,10 @@
+return {
+  "harrisoncramer/gitlab.nvim",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "dlyongemallo/diffview-plus.nvim",
+    "nvim-tree/nvim-web-devicons",
+  },
+  ---@type GitlabSettings
+  opts = {},
+}
